5f32f1cd88df76299845d11e02d03dff11df5e3f
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-03-18 21:50:11 +0200

    Add webpack alias for src directory in config

--- tec-frontend/next.config.js
+++ tec-frontend/next.config.js
@@ -1,4 +1,15 @@
+// tec-frontend/next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;

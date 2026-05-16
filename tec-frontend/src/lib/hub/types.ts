cb3567691c116aec5f573afad1c58a8cd2f8032e
Yasira82 <e23305707@gmail.com>
2026-05-16 21:34:33 +0300

    Add types for PiPrice and HubApp interfaces

---
+++ tec-frontend/src/lib/hub/types.ts
@@ -0,0 +1,14 @@
+export interface PiPrice {
+  price:    number;
+  change24h: number;
+  high24h:  number;
+  low24h:   number;
+}
+
+export interface HubApp {
+  slug:  string;
+  name:  string;
+  emoji: string;
+  href:  string;
+  desc:  string;
+}

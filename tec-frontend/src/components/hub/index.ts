4d8812ff8c6859f13fcc62b2f7a49bc8765f39c4
Yasira82 <e23305707@gmail.com>
2026-05-16 21:48:07 +0300

    Create index.ts

---
+++ tec-frontend/src/components/hub/index.ts
@@ -0,0 +1,6 @@
+export { HubHeader }      from './HubHeader';
+export { HubWalletCard }  from './HubWalletCard';
+export { HubCarousel }    from './HubCarousel';
+export { HubPayActions }  from './HubPayActions';
+export { HubAppsGrid }    from './HubAppsGrid';
+export { HubComingSoon }  from './HubComingSoon';
